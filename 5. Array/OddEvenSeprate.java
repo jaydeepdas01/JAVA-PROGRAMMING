19c1f431a93007ac7ab3e03e1458c1edb64b5b9a
jaydeepdas01 <97689637+jaydeepdas01@users.noreply.github.com>
2025-08-22 11:11:21 +0530

    updating folder and arrays to array

--- 5. Array/OddEvenSeprate.java
+++ 5. Array/OddEvenSeprate.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class oddEvenSeprate {
+public class OddEvenSeprate {
     public static void main(String[] args) {
         int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int[] arr2 = new int[arr1.length]; // for even
